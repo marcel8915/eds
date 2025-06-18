cb641a9527f3bae030925816ddef1417ede6e1ed
Syawal Hisham <syawal.hisham@aleph-labs.com>
2025-06-18 16:08:32 +0800

    ing changes

---
+++ blocks/grid-cards/grid-cards.js
@@ -0,0 +1,31 @@
+export default function decorate(block) {
+  console.log('Grid cards block loaded'); 
+  const container = document.createElement("div");
+  container.className = "grid-cards-container";
+
+  const wrapper = document.createElement("div");
+  wrapper.className = "grid-cards-wrapper";
+
+  const gridBlock = document.createElement("div");
+  gridBlock.className = "grid-cards block";
+
+  Array.from(block.children).forEach((card) => {
+    const hasContent =
+      card.textContent.trim() !== "" || card.querySelector("picture");
+    if (!hasContent) return;
+
+    const cardContainer = document.createElement("div");
+    cardContainer.className = "grid-card";
+
+    while (card.firstChild) {
+      cardContainer.appendChild(card.firstChild);
+    }
+
+    gridBlock.appendChild(cardContainer);
+  });
+
+  wrapper.appendChild(gridBlock);
+  container.appendChild(wrapper);
+  block.innerHTML = "";
+  block.appendChild(container);
+}
