221fe6c7574d26a8420188bed433455609fb7fb4
khai-rule <114408048+khai-rule@users.noreply.github.com>
2025-06-19 15:11:17 +0800

    Revert "Revert "Feat/slider teaser""

--- blocks/slider-teaser/slider-teaser.js
+++ blocks/slider-teaser/slider-teaser.js
@@ -1,46 +1,48 @@
-export default function decorate(block) {
-  const parallaxSections = block.querySelectorAll(".parallax-section");
+const parallaxSections = document.querySelector(
+  ".slider-teaser-media-container"
+);
 
-  parallaxSections.forEach((section) => {
-    const sections = section.querySelectorAll(".scroll-section");
-    const backgrounds = section.querySelectorAll(".sticky > .absolute");
-    let activeIndex = 0;
+console.log("parallaxSections", parallaxSections);
+parallaxSections.forEach((section) => {
+  const sections = section.querySelectorAll(".scroll-section");
+  const backgrounds = section.querySelectorAll(".sticky > .absolute");
+  let activeIndex = 0;
 
-    const handleScroll = () => {
-      let currentIndex = 0;
+  const handleScroll = () => {
+    let currentIndex = 0;
 
-      sections.forEach((scrollSection, index) => {
-        const rect = scrollSection.getBoundingClientRect();
-        if (rect.top <= window.innerHeight * 0.5) {
-          currentIndex = index;
-        }
-      });
-
-      if (currentIndex !== activeIndex) {
-        activeIndex = currentIndex;
-        updateBackgrounds();
+    sections.forEach((scrollSection, index) => {
+      const rect = scrollSection.getBoundingClientRect();
+      if (rect.top <= window.innerHeight * 0.5) {
+        currentIndex = index;
       }
-    };
+    });
 
-    const updateBackgrounds = () => {
-      backgrounds.forEach((background, index) => {
-        if (
-          index === activeIndex ||
-          (activeIndex >= sections.length - 1 && index === sections.length - 1)
-        ) {
-          background.style.opacity = "1";
-          background.style.zIndex = "1";
-        } else {
-          background.style.opacity = "0";
-          background.style.zIndex = "-1";
-        }
-      });
-    };
+    if (currentIndex !== activeIndex) {
+      activeIndex = currentIndex;
+      updateBackgrounds();
+    }
+  };
 
-    window.addEventListener("scroll", handleScroll);
-    window.addEventListener("resize", handleScroll);
+  const updateBackgrounds = () => {
+    backgrounds.forEach((background, index) => {
+      if (
+        index === activeIndex ||
+        (activeIndex >= sections.length - 1 && index === sections.length - 1)
+      ) {
+        background.style.opacity = "1";
+        background.style.zIndex = "1";
+      } else {
+        background.style.opacity = "0";
+        background.style.zIndex = "-1";
+      }
+    });
+  };
 
-    // Initialize the backgrounds
-    updateBackgrounds();
-  });
-}
+  window.addEventListener("scroll", handleScroll);
+  window.addEventListener("resize", handleScroll);
+
+  // Initialize the backgrounds
+  updateBackgrounds();
+});
+export default function decorate(block) {}
