83dead0da2c16fd178add866ad0d602949494c10
marcel8915 <white.cappucino@gmail.com>
2025-07-14 12:08:10 +0700

    test

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,15 +1,15 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
-export default function decorate(block) {console.log('block', block);
+export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {console.log('div', div);
-      if (div.querySelector('picture')) div.className = 'cards-card-image';
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
     });
     ul.append(li);
--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,3 +1,7 @@
 export default function decorate(block) {
     console.log(block);
+    const rows = block.querySelectorAll(':scope > div');
+    rows.forEach((row) => {
+        console.log('row', row);
+    });
 }
